c696ba9713728ce4e2d8511beda99a0ee6192505
Calley Chai <z5366974@ad.unsw.edu.au>
2023-03-24 20:59:35 +1100

    feat: added starter code and instructions to goldguess.s

---
+++ goldguess.s
@@ -0,0 +1,34 @@
+# -- Register Notes --
+# $t0 = 
+# $t1 = 
+# $t2 = 
+# ...
+
+# Constant Declarations
+CORRECT_GUESS = 12			# Replace me with the amount of gold you want
+
+
+# Where you write your program code
+# Note that every program needs a main: label
+.text
+main:
+
+guess__loop_init:
+
+
+guess__loop_cond:
+
+
+guess__loop_body:
+
+
+guess__loop_increment:
+
+
+main__epilogue:
+	li	$v0, 0
+	jr 	$ra
+
+# Write the strings you want to print here
+# .data is the section where data is stored in main memory (RAM)
+.data
