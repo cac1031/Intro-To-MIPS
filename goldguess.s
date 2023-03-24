b973afc06eb4f31104c7bf4612dac477eb8e0950
Calley Chai <z5366974@ad.unsw.edu.au>
2023-03-24 21:11:08 +1100

    docs: added comments representing instructions in gold_guess.s

--- goldguess.s
+++ goldguess.s
@@ -13,17 +13,20 @@ CORRECT_GUESS = 12			# Replace me with the amount of gold you want
 # Add more labels!
 .text
 main:
+# TODO (Task 1): Print a message from No-Face
 
-guess__loop_init:
+# TODO (Task 2): Scan in a number representing guess for gold nugget amount
 
+# TODO (Task 3): Evaluate guess
+# - If guess is too low, tell Sen to guess higher
+# - If guess is too high, tell Sen to guess lower
+# - If guess is equal to correct amount, tell Sen she won!
 
-guess__loop_cond:
-
-
-guess__loop_body:
-
-
-guess__loop_increment:
+# TODO (Task 4): Now change it so that Sen has 3 guesses instead of 1
+# Note that if Sen guesses a non-positive integer, she can't make further guesses. Print an appropriate message
+# Similar to Task 3, print a message if each guess is too low or high
+# If Sen guesses correct, we stop the loop and print a message saying she won!
+# If Sen majkes 3 wrong guesses, we print a losing message
 
 
 main__epilogue:
