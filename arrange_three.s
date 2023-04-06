84842f3aa40550108a6e8e0d1419f292f4283185
CHUN Nicole Wai Yan <nicole.chun@compclub.org>
2023-04-07 01:31:58 +1000

    set up student files

--- arrange_three.s
+++ arrange_three.s
@@ -1,1 +1,22 @@
+# This program scans in three numbers and prints them in ascending order, 
+# separated by a comma and a space
 
+# Written by: <write your name here and remove triangular brackets>
+
+###############################################################################
+# .text: Where you write your program code
+.text
+main:
+# TODO: Add code here
+
+main__epilogue:
+	li	$v0, 0		# return 0
+	jr 	$ra
+
+###############################################################################
+# .data: The section where data is stored in main memory (RAM)
+.data
+first_num_str:		.asciiz "Enter first number: "
+second_num_str:		.asciiz "Enter second number: "
+third_num_str:		.asciiz "Enter third number: "
+arranged_num_str:	.asciiz "Arranged Numbers: "
---
+++ movie_day.s
@@ -0,0 +1,25 @@
+# This program takes in a number between 1 - 7, denoting the days of the week, 
+#and depending on the number, gives a movie for the corresponding day of the week
+
+# Written by: <write your name here and remove triangular brackets>
+
+###############################################################################
+# .text: Where you write your program code
+.text
+main:
+# TODO: Add code here
+
+main__epilogue:
+	li	$v0, 0		# return 0
+	jr 	$ra
+
+###############################################################################
+# .data: The section where data is stored in main memory (RAM)
+.data
+enter_day_str:			.asciiz "Enter day: "
+invalid_day_str:		.ascii  "Invalid day!\n"
+movie_to_watch_str:		.asciiz "Movie to watch: "
+
+movie1_str:			.asciiz "Princess Mononoke\n" # (objectively the best studio ghibli film)
+
+# TODO: Add more movie strings for the rest of the days of the week
